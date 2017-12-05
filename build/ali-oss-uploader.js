99e27ea2a7258dd38b66f2b7da89c46e7a4d1342
Wallace Mao <maowenqiang0752@163.com>
2017-12-05 17:24:58 +0800

    add part upload to ali-oss-uploader

--- build/ali-oss-uploader.js
+++ build/ali-oss-uploader.js
@@ -18,6 +18,8 @@ var roleArn = secret.aliOSS.roleArn
 var sessionName = 'dingtalkFrontUser'
 var expiration = 900
 
+var uploadRetryTimes = 5
+
 var argv = process.argv
 if(argv.length < 3){
   return console.log(chalk.white.bgRed(' ERROR '), chalk.red('wrong number of argument, please add the upload directory'))
@@ -46,16 +48,37 @@ co(function* () {
     var relativePath = path.relative(uploadDir, file)
     var mills = new Date().getTime()
     var uploadName = url.resolve(ossRootPath, relativePath)
-    var res = yield client.put(uploadName, file);
-    if(res.res.status == 200){
-      console.log(chalk.blue('>>---' + (new Date().getTime() - mills) + 'ms, ' + relativePath + ' --> ' + uploadName))
-    } else {
-      console.log(chalk.white.bgRed(' ERROR '), chalk.red(file + ' --> ' + uploadName))
-      console.log(res)
-      break
+
+    var checkpoint = null;
+    for (var j = 0; j < uploadRetryTimes; j++) {
+      try {
+        var res = yield client.multipartUpload(uploadName, file, {
+          checkpoint: checkpoint,
+          progress: function* (p, cpt){
+            checkpoint = cpt
+            console.log(chalk.blue('>>---' + Math.ceil(p * 100) + '% : ' + (new Date().getTime() - mills) + 'ms, ' + relativePath + ' --> ' + uploadName))
+          }
+        });
+        console.log(chalk.green('||---done, ' + (new Date().getTime() - mills) + 'ms, ' + relativePath + ' --> ' + uploadName))
+        break
+      } catch (err) {
+        if (j < uploadRetryTimes - 1) {
+          console.log(chalk.red(' WARN retry: ' + (j + 1) + ' '), chalk.red(relativePath + ' --> ' + uploadName), chalk.red(err))
+        } else {
+          console.log(chalk.white.bgRed(' ERROR '), chalk.red(relativePath + ' --> ' + uploadName), chalk.red(err))
+        }
+      }
     }
+
+    // if(res.res.status == 200){
+    //   console.log(chalk.blue('>>---' + (new Date().getTime() - mills) + 'ms, ' + relativePath + ' --> ' + uploadName))
+    // } else {
+    //   console.log(chalk.white.bgRed(' ERROR '), chalk.red(file + ' --> ' + uploadName))
+    //   console.log(res)
+    //   break
+    // }
   }
-  console.log(chalk.blue('>>finish upload'))
+  // console.log(chalk.blue('>>finish upload'))
 }).catch(function (err) {
   console.log(err);
 });
